9418147964dc380a698d9f5c4e3b5eb94b79b8fd
amiteshore <amiteshore@gmail.com>
2026-05-19 22:07:25 +0530

    feat: fixed tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,15 +2,19 @@ import { TabBarIcon } from "@/components/ui/TabBarIcon";
 import { useAuth } from "@/context/AuthContext";
 import { Redirect, Tabs } from "expo-router";
 import { Platform } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TeacherTabLayout() {
   const { user, loading } = useAuth();
+  const insets = useSafeAreaInsets();
 
   if (loading) return null;
 
   if (!user || user.role !== "teacher") {
     return <Redirect href="/(auth)/login" />;
   }
+
+  const bottomPadding = Platform.OS === "ios" ? insets.bottom : insets.bottom + 8;
 
   return (
     <Tabs
@@ -24,17 +28,16 @@ export default function TeacherTabLayout() {
           fontWeight: "600",
         },
         tabBarStyle: {
-          height: Platform.OS === "ios" ? 74 : 66,
+          height: 56 + bottomPadding,
           backgroundColor: "#FFFFFF",
           borderTopWidth: 0.5,
-
           elevation: 0,
           shadowColor: "#0F172A",
           shadowOpacity: 0.08,
-          shadowOffset: { width: 0, height: 10 },
-          shadowRadius: 20,
+          shadowOffset: { width: 0, height: -2 },
+          shadowRadius: 10,
           paddingTop: 8,
-          paddingBottom: Platform.OS === "ios" ? 16 : 8,
+          paddingBottom: bottomPadding,
         },
       }}
     >
